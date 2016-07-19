54be1547e47645beef7bdfb5606ad2ce0673ddf0
jamiemcl <jamie@jamiemc.co.uk>
2016-07-19 21:20:27 +0100

    task 2 changes

---
+++ Dezrez.Rezi.DataContracts/Events/EventDataContract.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dezrez.Rezi.Domain;
+using Dezrez.Rezi.Domain.Roles;
+
+namespace Dezrez.Rezi.DataContracts.Events
+{
+    public class EventDataContract : BaseDataContract
+    {
+        public List<Role> Roles { get; set; }
+    }
+}
